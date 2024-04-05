652aa68c6d1423d63e3cb2a3dde81ff005c11115
ron180420 <parmarrohan.13@gmail.com>
2024-04-05 19:47:18 +0530

    create tours to study usestate and useeffect hooks

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Birthday from '../birthday/Birthday.jsx'
+import Apps from '../tours/Apps.jsx'
 const App = () => {
   return (
     <div>
-    <Birthday />
+    <Apps />
     </div>
   )
 }
