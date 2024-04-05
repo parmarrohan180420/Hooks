67c9f32e356dcca9a0496f8e8af3dca86fd507e1
ron180420 <parmarrohan.13@gmail.com>
2024-04-05 22:50:57 +0530

    created a review app

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Birthday from '../birthday/Birthday.jsx'
-import Apps from '../tours/Apps.jsx'
+import Apps from '../Review/Apps.jsx'
 const App = () => {
   return (
     <div>
